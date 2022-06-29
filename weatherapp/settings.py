4142af0cfeb52b99d2b6649e28c8d2daaa72293a
ketanu0690 <78308610+ketanu0690@users.noreply.github.com>
2022-06-29 18:37:21 +0530

    seting

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
-from whitenoise.django import DjangoWhiteNoise
+from whitenoise import DjangoWhiteNoise
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
